445a688c470b16288102128c5723b7783e32e1dd
Wyltre <140523998+Wyltre@users.noreply.github.com>
2024-05-18 20:16:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,39 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+var users = {}; // Kullanıcı adlarını saklamak için nesne
 
-let users = {};
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
+io.on("connection", function(socket) {
   console.log("a user connected");
 
-  socket.on('set username', (username) => {
+  socket.on('set username', function(username) {
     if (!users[username]) {
-      users[username] = socket.id;
-      socket.username = username;
-      socket.emit('username set', username);
-      io.emit('user list', Object.keys(users));
+      users[username] = true; // Kullanıcı adı kullanılmıyorsa sakla
+      socket.emit('username set', username); // Kullanıcı adını doğrula
     } else {
-      socket.emit('username exists');
+      socket.emit('username exists'); // Kullanıcı adı kullanılıyorsa hata mesajı gönder
     }
   });
 
-  socket.on('join general', () => {
-    socket.join('general');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', { user: users[socket.id], message: msg });
   });
 
-  socket.on('get users', () => {
-    socket.emit('user list', Object.keys(users));
+  socket.on('typing', function(data) {
+    socket.broadcast.emit('display', { user: users[socket.id], typing: data.typing });
   });
 
-  socket.on('chat message', (data) => {
-    if (data.type === 'general') {
-      io.to('general').emit('chat message', data);
-    } else if (data.type === 'private') {
-      if (users[data.id]) {
-        io.to(users[data.id]).emit('chat message', data);
-        io.to(users[data.user]).emit('chat message', data);
-      }
-    }
-  });
-
-  socket.on('typing', (data) => {
-    if (data.type === 'general') {
-      socket.broadcast.to('general').emit('display', data);
-    } else if (data.type === 'private') {
-      if (users[data.id]) {
-        socket.to(users[data.id]).emit('display', data);
-      }
-    }
-  });
-
-  socket.on("disconnect", () => {
-    if (socket.username) {
-      delete users[socket.username];
-      io.emit('user list', Object.keys(users));
-    }
-    console.log("user disconnected");
+  socket.on('disconnect', function() {
+    delete users[socket.id]; // Kullanıcı bağlantıyı kapatırsa kullanıcı adını sil
   });
 });
 
-server.listen(3000, () => {
+http.listen(3000, function() {
   console.log("listening on *:3000");
 });
